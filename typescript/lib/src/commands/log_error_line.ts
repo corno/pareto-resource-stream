58dfef295aefbac63c01dbbaa04299c0f24cd80e
Corno Schraverus <corno@schraverus.com>
2026-07-30 09:07:55 +0200

    pdt: ...

--- typescript/lib/src/commands/log_error_line.ts
+++ typescript/lib/src/commands/log_error_line.ts
@@ -1,5 +1,4 @@
 import * as p_ from 'pareto-core/implementation/resource'
-import p_text_from_list from 'pareto-core/implementation/transformer/specials/text_from_list'
 
 //interface
 import * as interface_ from "pareto-stream-api/interface/commands"
